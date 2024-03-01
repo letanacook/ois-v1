686a260b98a074aec9d93e2522d42b8bbca06060
nunii2 <um33822@student.uni-lj.si>
2024-03-01 15:18:06 +0100

    Pripravljena funkcija za izpis

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,8 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
     }
+	public static void izpis(double visina) {
+		System.out.printf("Gravitacijski pospesek na %.2f km nadmorske visine je
+		%.2f m/s^2\n", visina, izracunGravitacije(visina * 1000));
+	}
 }
