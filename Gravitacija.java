126c16daaf3375901f1f2dceb65cea62e8b85f24
nicknick75 <nika.kralj27@gmail.com>
2024-03-02 02:38:20 +0100

    Dodana funkcija za izračun

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,12 @@
 public class Gravitacija {
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+        System.out.println("OIS je zakon! Se strinjam.");
     }
+	
+	public static double izracunGravitacije(double visina) {
+		double C = 6.674e-11;
+		double M = 5.972e24;
+		double r = 6.371e6;
+		return (C * M) / Math.pow(r + visina, 2.0);
+	}
 }
